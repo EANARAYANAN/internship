5ae8e0cd8512e7f3b3b1a22b3cdccbb45f39c1ef
EANARAYANAN <ananthnarayanan317@gmail.com>
2016-03-14 13:50:06 +0530

    Update dictionary_pgm2.py

--- dictionary_pgm2.py
+++ dictionary_pgm2.py
@@ -1,10 +1,10 @@
 import re
-parag=raw_input("Enter the paragraph")
+parag=" I am a book. I can be read. I am a book"
 word1=re.findall(r"[\w']+|[.,!?;]", parag )
 dict={}
 for word in word1:
     try:
-    	dict[word] += 1
+        dict[word] += 1
     except KeyError:
-    	dict[word] = 1
+        dict[word] = 1
 print (dict)
